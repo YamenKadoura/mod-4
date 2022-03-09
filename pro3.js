be272d3518c5e86b6ed5cac39191ba5c3fd98fae
YamenKadoura <99257135+YamenKadoura@users.noreply.github.com>
2022-03-09 09:59:38 +0200

    Add files via upload

---
+++ pro3.js
@@ -0,0 +1,12 @@
+var a = ["Marcel","Dana","John","jad","Diana"]
+for (var i =0; i<a.length;i++)
+{
+    if (a[i][0]==="J" || a[i][0]==="j")
+    {
+        console.log("GoodBye J" + a[i])
+    }
+    else
+    {
+        console.log("Hello " + a[i])
+    }
+}
